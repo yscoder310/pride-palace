6640c8e6e9f2665f68c66999357b343284714937
rajan-programmic-access-at-136112644394 <rajan-programmic-access-at-136112644394>
2024-03-20 18:21:41 +0530

    first commit

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
